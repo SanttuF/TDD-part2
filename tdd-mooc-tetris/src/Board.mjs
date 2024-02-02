334fe340a00a784206593795dc83ecb85d1de567
SanttuF <santtu.f@gmail.com>
2024-02-03 00:18:00 +0200

    test passed

--- tdd-mooc-tetris/src/Board.mjs
+++ tdd-mooc-tetris/src/Board.mjs
@@ -7,7 +7,7 @@ export class Board {
     this.height = height;
     this.board = new Array(this.height).fill(null).map(() => new Array(this.width).fill('.'))
     this.falling = false
-    this.c = []
+    this.current = []
   }
 
   toString() {
@@ -23,34 +23,34 @@ export class Board {
     const boardCenter = Math.floor(this.board.length/2) + 1
     const blockOffset = boardCenter - Math.floor(block[0].length)
 
-    this.c = []
+    this.current = []
 
     for (let i = 0; i < aBlock.length; i++) {
       for (let j = 0; j < aBlock[0].length; j++) {
         this.board[i][j + blockOffset] = aBlock[i][j]
         if (aBlock[i][j] !== '.') {
-          this.c.push([i, j + blockOffset])
+          this.current.push([i, j + blockOffset])
         }
       }
     }
-    
+
     this.falling = true
   }
 
   tick() {
     const newBoard = this.board.map(a => a.slice())
 
-    this.c.forEach(e => newBoard[e[0]][e[1]] = '.')
+    this.current.forEach(e => newBoard[e[0]][e[1]] = '.')
 
-    for (let index = 0; index < this.c.length; index++) {
-      const [i, j] = this.c[index]
+    for (let index = 0; index < this.current.length; index++) {
+      const [i, j] = this.current[index]
       if (i+1 >= this.height || (this.board[i+1][j] !== '.' && !this.#arrayInC([i+1, j]))){
         this.falling = false
         return
       }
 
       newBoard[i+1][j] = this.board[i][j]
-      this.c[index] = [i+1, j]
+      this.current[index] = [i+1, j]
     }
     this.board = newBoard
   }
@@ -59,7 +59,7 @@ export class Board {
     return this.falling
   }
   #arrayInC(i) {
-    return this.c.some(e => JSON.stringify(e) === JSON.stringify(i))
+    return this.current.some(e => JSON.stringify(e) === JSON.stringify(i))
   }
 }
 
