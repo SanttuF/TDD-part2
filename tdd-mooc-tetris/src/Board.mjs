9de03fcd8b20ceaaf323ce3a1a78874e5b51c8a1
SanttuF <santtu.f@gmail.com>
2024-02-03 00:13:13 +0200

    test passed

--- tdd-mooc-tetris/src/Board.mjs
+++ tdd-mooc-tetris/src/Board.mjs
@@ -23,29 +23,30 @@ export class Board {
     const boardCenter = Math.floor(this.board.length/2) + 1
     const blockOffset = boardCenter - Math.floor(block[0].length)
     this.c = []
-
     for (let i = 0; i < aBlock.length; i++) {
       for (let j = 0; j < aBlock[0].length; j++) {
         this.board[i][j + blockOffset] = aBlock[i][j]
-        this.c.push([i, j + blockOffset])
+        if (aBlock[i][j] !== '.') {
+          this.c.push([i, j + blockOffset])
+        }
       }
     }
     this.falling = true
   }
 
   tick() {
-    const newBoard = this.board
-    this.c.forEach((e, index) => {
-      const [i, j] = e
-      if (i+1 >= this.height || (this.board[i+1][j] !== '.' )){
+    const newBoard = this.board.map(a => a.slice())
+    this.c.forEach(e => newBoard[e[0]][e[1]] = '.')
+    for (let index = 0; index < this.c.length; index++) {
+      const [i, j] = this.c[index]
+      if (i+1 >= this.height || (this.board[i+1][j] !== '.' && !this.#arrayInC([i+1, j]))){
         this.falling = false
         return
       }
 
       newBoard[i+1][j] = this.board[i][j]
-      newBoard[i][j] = '.'
       this.c[index] = [i+1, j]
-    })
+    }
     this.board = newBoard
   }
 
