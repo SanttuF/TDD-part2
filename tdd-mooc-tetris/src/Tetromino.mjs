ddee759f7866ae35b1a3a7953a441da4a428beec
SanttuF <santtu.f@gmail.com>
2024-02-03 02:29:23 +0200

    test passed

--- tdd-mooc-tetris/src/Tetromino.mjs
+++ tdd-mooc-tetris/src/Tetromino.mjs
@@ -19,5 +19,14 @@ export class Tetromino {
         }
         return new Tetromino(newShape.map(e => e.join('')).join('\n'))
     }
-    
+    rotateLeft() {
+        const newShape = []
+        for (let i = 0; i < this.shape.length; i++) {
+            newShape.push([])
+            for (let j = 0; j < this.shape.length; j++) {
+                newShape[i].push(this.shape[j][this.shape.length - i - 1])
+            }
+        }
+        return new Tetromino(newShape.map(e => e.join('')).join('\n'))
+    }
 }
