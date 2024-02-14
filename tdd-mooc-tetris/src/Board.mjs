54f4cf092ddbc984ad99dce4862775d39f0a64d4
SanttuF <santtu.f@gmail.com>
2024-02-14 04:15:43 +0200

    test passed

--- tdd-mooc-tetris/src/Board.mjs
+++ tdd-mooc-tetris/src/Board.mjs
@@ -73,6 +73,10 @@ export class Board {
     this.#move('right')
   }
 
+  moveDown() {
+    this.#move('down')
+  }
+
   #move(dir) {
     const newBoard = this.board.map(a => a.slice())
     this.current.forEach(e => newBoard[e[0]][e[1]] = '.')
@@ -84,6 +88,7 @@ export class Board {
       switch (dir) {
         case 'left': newJ -= 1; break
         case 'right': newJ += 1; break
+        case 'down': newI += 1; break
         default: throw new Error('Invalid direction')
       }
       if (this.#checkCollision(newI, newJ)) return
