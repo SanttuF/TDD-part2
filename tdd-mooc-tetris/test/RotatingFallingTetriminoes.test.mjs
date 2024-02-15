bd73d41a1c6de60b12807c4e217f476c9e2ee0f3
SanttuF <santtu.f@gmail.com>
2024-02-15 05:20:59 +0200

    added test

---
+++ tdd-mooc-tetris/test/RotatingFallingTetriminoes.test.mjs
@@ -0,0 +1,25 @@
+import { beforeEach, describe } from "vitest";
+import { Board } from "../src/Board.mjs";
+import { Tetromino } from "../src/Tetromino.mjs";
+import { expect } from "chai";
+
+describe('Falling tetriminoe rotation', () => {
+    let board
+    beforeEach(() => {
+        board = new Board(10, 6)
+    })
+
+    test('Falling tetriminoes can be rotated clockwise', () => {
+        board.drop(Tetromino.T_SHAPE)
+        board.rotateRight()
+
+        expect(board.toString()).to.equalShape(
+           `....T.....
+            ....TT....
+            ....T.....
+            ..........
+            ..........
+            ..........`
+        )
+    })
+})
