dd9d2dcab2fb26589f0446f6fbbabbfaa8fc0297
SanttuF <santtu.f@gmail.com>
2024-02-18 19:58:22 +0200

    test passed

--- tdd-mooc-tetris/src/Board.mjs
+++ tdd-mooc-tetris/src/Board.mjs
@@ -96,6 +96,7 @@ export class Board {
     //   this.eraseBlock(i, j)
     //   this.drawBlock
     // }
+    
     const newBoard = this.board.map(a => a.slice())
     this.current.forEach(e => newBoard[e[0]][e[1]] = '.')
     const newCurrent = this.current.map(a => a.slice())
