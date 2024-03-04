40003567a1181a5eed30ff25c9e2f4c39a3b8c90
SanttuF <santtu.f@gmail.com>
2024-03-04 02:52:51 +0200

    added test

---
+++ tdd-mooc-tetris/test/ClearLine.test.mjs
@@ -0,0 +1,39 @@
+import { beforeEach, describe, test } from "vitest";
+import { Tetromino } from "../src/Tetromino.mjs";
+import { Board } from "../src/Board.mjs";
+import { expect } from "chai";
+
+function fallToBottom(board) {
+    for (let i = 0; i < 10; i++) {
+      board.tick();
+    }
+}
+
+describe('Line clear and block falling', () => {
+
+    let board
+    beforeEach(() => {
+        board = new Board(10, 6)
+    })
+
+    test('Line clears when its full', () => {
+        board.board = [
+        ['.','.','.','.','.','.','.','.','.','.'],
+        ['.','.','.','.','.','.','.','.','.','.'],
+        ['.','.','.','.','.','.','.','.','.','.'],
+        ['.','.','.','.','.','.','.','.','.','.'],
+        ['L','L','L','L','.','.','L','L','L','L'],
+        ['L','L','L','L','.','.','L','L','L','L'],]
+
+        board.drop(Tetromino.O_SHAPE)
+        fallToBottom(board)
+        expect(board.toString()).to.equalShape(
+           `..........
+            ..........
+            ..........
+            ..........
+            ..........
+            ..........`
+        )
+    })
+})
