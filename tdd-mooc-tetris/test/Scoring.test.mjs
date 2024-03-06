1ed0b968d6864f1bb709c93bb7bc76a5e1cd01f6
SanttuF <santtu.f@gmail.com>
2024-03-06 02:20:44 +0200

    test passed

--- tdd-mooc-tetris/test/Scoring.test.mjs
+++ tdd-mooc-tetris/test/Scoring.test.mjs
@@ -6,10 +6,11 @@ import { ScoringSystem } from "../src/Scoring.mjs";
 
 describe('Scoring system', () => {
     let board
-    let score
+    let score = ScoringSystem
     beforeEach(()=> {
-        score = ScoringSystem
-        board = new Board(4,3, score)
+        score.score = 0
+        score.level = 0
+        board = new Board(4,4, score)
     })
 
     test('score increases', () => {
@@ -17,7 +18,35 @@ describe('Scoring system', () => {
         board.tick()
         board.tick()
         board.tick()
+        board.tick()
 
         expect(score.score).to.equal(40)
+    })
+
+    test('score increases based on rows cleared', () => {
+        board.drop(Tetromino.O_SHAPE)
+        board.moveLeft()
+        board.tick()
+        board.tick()
+        board.tick()
+
+        board.drop(Tetromino.O_SHAPE)
+        board.moveRight()
+        board.tick()
+        board.tick()
+        board.tick()
+
+        expect(score.score).to.equal(100)
+    })
+
+    test('score increases based on level', () => {
+        score.level = 2
+        board.drop(Tetromino.I_SHAPE)
+        board.tick()
+        board.tick()
+        board.tick()
+        board.tick()
+
+        expect(score.score).to.equal(120)
     })
 })
